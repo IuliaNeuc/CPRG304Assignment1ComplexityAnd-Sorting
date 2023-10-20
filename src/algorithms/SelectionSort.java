3add09fc057490412c4178f27a99ff750979ca1c
sergei137 <107164155+sergei137@users.noreply.github.com>
2023-10-20 14:45:20 -0600

    more error fixes

--- src/algorithms/SelectionSort.java
+++ src/algorithms/SelectionSort.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 public class SelectionSort
 {
     //Selection sort using the natural ordering of elements - Comparable
-    public static <T extends Comparable<? super T>> void SelectionSort(T[] array)
+    public static <T extends Comparable<? super T>> void selectionSort(T array[])
     {
         int x = array.length;
         
@@ -32,7 +32,7 @@ public class SelectionSort
             }
 
             // // After the inner loop, 'large' contains the largest element in the remaining unsorted portion
-            int res2 = large.compareTo(array[i])
+            int res2 = large.compareTo(array[i]);
 
             // If 'large' is greater than the current element at index 'i', swap them
             if(res2 > 0 )
@@ -45,14 +45,14 @@ public class SelectionSort
     }
 
     //Selection sort with a custom Comparator
-    public static <T> void SelectionSort(T[] array, Comparator<? super T> c)
+    public static <T> void selectionSort(T[] array, Comparator<? super T> c)
     {
         int x = array.length;
         if (x == 1) return; // If the array has only one element, it is already sorted.
 
         for(int i = 0 ; i < x; i++)
         {
-            T large = array[i]
+            T large = array[i];
             int largeIndex = i;
 
             for(int j = i; j < x; j++)
