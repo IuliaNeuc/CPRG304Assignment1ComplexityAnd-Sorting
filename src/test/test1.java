8bdf1a93ef05a828f6a26c905e65b8ae0d18c423
sergei137 <107164155+sergei137@users.noreply.github.com>
2023-10-20 14:56:36 -0600

    finally started this boyo

---
+++ src/test/test1.java
@@ -0,0 +1,13 @@
+package test;
+
+public class test1 {
+    
+}
+
+
+
+// The compare type will be provided as input from the command line to your program: h for height, v for volume, and a for base area via the -t option.
+
+// Write a testing program that will read a text file (entered at the command line via the -f option) of random shapes that adds them to an array (not ArrayList).
+
+
