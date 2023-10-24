620a5470eba50f2ab593796e46050b4f04c3ea57
sergei137 <107164155+sergei137@users.noreply.github.com>
2023-10-24 13:35:39 -0600

    completely reworked file reading from array lists to just array, still working in it

--- src/filereading/ReadFiles.java
+++ src/filereading/ReadFiles.java
@@ -7,8 +7,7 @@ import java.util.ArrayList;
 public class ReadFiles {
     public static void ReadFile(File fileToRead) {
 
-        // shape array lists
-        ArrayList<String> shapeList = new ArrayList<String>(); // all shapes
+        // shape array lists - DELETE THESE
         ArrayList<String> cylinderList = new ArrayList<String>(); 
         ArrayList<String> coneList = new ArrayList<String>();
         ArrayList<String> pyramidList = new ArrayList<String>();
@@ -17,57 +16,71 @@ public class ReadFiles {
         ArrayList<String> pentagonalPrismList = new ArrayList<String>();
         ArrayList<String> octagonalPrismList = new ArrayList<String>();
 
+        String[] shapeArray = null; // all shapes
+        ArrayList<String> cylinderArray = null;
+        ArrayList<String> coneArray = null;
+        ArrayList<String> pyramidArray = null;
+        ArrayList<String> triangularPrismArray = null;
+        ArrayList<String> squarePrismArray = null;
+        ArrayList<String> pentagonalPrismArray = null;
+        ArrayList<String> octagonalPrismArray = null;
+        int elementCount = 0;
+        
         // read file
         try (BufferedReader reader = new BufferedReader(new java.io.FileReader(fileToRead))) {
-            String line;
-            while ((line = reader.readLine()) != null) {   
-                String[] elements = line.split(" ");
-                for (String element1 : elements) {
-                    shapeList.add(element1);
-                }
+            
+            // read first line (only works if there is one line in the file)
+            String line = reader.readLine(); // read line
+            shapeArray = line.split(" "); // split line into array by spaces
+            elementCount = (Integer.parseInt(shapeArray[0]) * 3) + 1; // convert first element to int and multiply by 3 to get total number of elements
+            
+            // test print statements
+            System.out.println("Object count: " + elementCount);
+            for (int i = 0; i < elementCount; i++) {
+                System.out.println("shapeArray: " + shapeArray[i]);
             }
-            shapeList.remove(0); // removes shape count integer from beginning of file  -- NEED THIS CODE
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
 
         // sort shapes into respective array lists
-        for (int i = 0; i < shapeList.size() - 2; i++) {                
-            switch (shapeList.get(i)) {
+        for (int i = 0; i < elementCount - 2; i++) { 
+            switch (shapeArray[i]) {
                 case "Cone":
-                    coneList.add(shapeList.get(i));
-                    coneList.add(shapeList.get(i + 1));
-                    coneList.add(shapeList.get(i + 2));
+
+                    coneList.add(shapeArray[i]);
+                    coneList.add(shapeArray[i + 1]);
+                    coneList.add(shapeArray[i + 2]);
                     break;
                 case "Cylinder":
-                    cylinderList.add(shapeList.get(i));
-                    cylinderList.add(shapeList.get(i + 1));
-                    cylinderList.add(shapeList.get(i + 2));
+                    cylinderList.add(shapeArray[i]);
+                    cylinderList.add(shapeArray[i + 1]);
+                    cylinderList.add(shapeArray[i + 2]);
                     break;
                 case "Pyramid":
-                    pyramidList.add(shapeList.get(i));
-                    pyramidList.add(shapeList.get(i + 1));
-                    pyramidList.add(shapeList.get(i + 2));
+                    pyramidList.add(shapeArray[i]);
+                    pyramidList.add(shapeArray[i + 1]);
+                    pyramidList.add(shapeArray[i + 2]);
                     break;
                 case "TriangularPrism":
-                    triangularPrismList.add(shapeList.get(i));
-                    triangularPrismList.add(shapeList.get(i + 1));
-                    triangularPrismList.add(shapeList.get(i + 2));
+                    triangularPrismList.add(shapeArray[i]);
+                    triangularPrismList.add(shapeArray[i + 1]);
+                    triangularPrismList.add(shapeArray[i + 2]);
                     break;
                 case "SquarePrism":
-                    squarePrismList.add(shapeList.get(i));
-                    squarePrismList.add(shapeList.get(i + 1));
-                    squarePrismList.add(shapeList.get(i + 2));
+                    squarePrismList.add(shapeArray[i]);
+                    squarePrismList.add(shapeArray[i + 1]);
+                    squarePrismList.add(shapeArray[i + 2]);
                     break;
                 case "PentagonalPrism":
-                    pentagonalPrismList.add(shapeList.get(i));
-                    pentagonalPrismList.add(shapeList.get(i + 1));
-                    pentagonalPrismList.add(shapeList.get(i + 2));
+                    pentagonalPrismList.add(shapeArray[i]);
+                    pentagonalPrismList.add(shapeArray[i + 1]);
+                    pentagonalPrismList.add(shapeArray[i + 2]);
                     break;
                 case "OctagonalPrism":
-                    octagonalPrismList.add(shapeList.get(i));
-                    octagonalPrismList.add(shapeList.get(i + 1));
-                    octagonalPrismList.add(shapeList.get(i + 2));
+                    octagonalPrismList.add(shapeArray[i]);
+                    octagonalPrismList.add(shapeArray[i + 1]);
+                    octagonalPrismList.add(shapeArray[i + 2]);
                     break;
                 default:
                     break;
@@ -76,7 +89,13 @@ public class ReadFiles {
 
         // test print statements
         int totalSize = coneList.size() + cylinderList.size() + pyramidList.size() + squarePrismList.size() + triangularPrismList.size() + pentagonalPrismList.size() + octagonalPrismList.size();
-        System.out.println("Total size: " + totalSize);
-        System.out.println("shape list size: " + shapeList.size());
+        System.out.println("Sorted List size: " + totalSize);
+        System.out.println("shapeArray size: " + shapeArray.length);
+        for (int i = 0; i < coneList.size(); i++) {
+            System.out.println("coneList: " + coneList.get(i));
+        }
+        for (int i = 0; i < cylinderList.size(); i++) {
+            System.out.println("cylinderList: " + cylinderList.get(i));
+        }
     }   
 }
