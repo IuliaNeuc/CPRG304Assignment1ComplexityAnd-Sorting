69ebf2708ea561d2632637efbff904fcde833bef
Sergei137 <sm1.618@protonmail.com>
2023-10-25 17:29:45 -0600

    removed test code from App.java

--- src/application/App.java
+++ src/application/App.java
@@ -101,44 +101,6 @@ public class App {
         PentagonalPrism[] pentagonalPrismArray = (PentagonalPrism[]) shapeObjectArray[6];
         OctagonalPrism[] octagonalPrismArray = (OctagonalPrism[]) shapeObjectArray[7];
 
-        // test code for sorting
-        Cone cone = new Cone(674.2435, 652.1534);
-        Cylinder cylinder = new Cylinder(9431.453, 4450.123);
-
-        // cannot use ArrayList
-        ArrayList<Shape> shapes = new ArrayList<Shape>();
-        shapes.add(cylinder);
-        shapes.add(cone);
-
-        Collections.sort(shapes);
-        for (Shape l : shapes) {
-            System.out.println(l);
-        }
-    
-
-        Comparator<Shape> comparator = new Comparator<Shape>() {
-            @Override
-            public int compare(Shape s1, Shape s2) {
-                double area1 = s1.calcBaseArea();
-                double area2 = s2.calcBaseArea();
-                if (area1 < area2) {
-                    return -1;
-                } else if (area1 > area2) {
-                    return 1;
-                } else {
-                    double volume1 = s1.calcVolume();
-                    double volume2 = s2.calcVolume();
-                    if (volume1 < volume2) {
-                        return -1;
-                    } else if (volume1 > volume2) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                }
-            }
-        };
-        Collections.sort(shapes, comparator);
     }
 }
 
