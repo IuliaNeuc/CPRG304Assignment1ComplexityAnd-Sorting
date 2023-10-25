58de3954272e820cbc7a72c8f355dc5df67993be
Sergei137 <sm1.618@protonmail.com>
2023-10-24 21:09:23 -0600

    untested cmd line args implemented

--- src/application/App.java
+++ src/application/App.java
@@ -14,72 +14,84 @@ import filereading.ReadFiles;
 public class App {
     public static void main(String[] args) {
 
-        Scanner consoleScanner = new Scanner(System.in); // open scanner 
+        // variables for command line arguments
+        String filePath = ""; // file name
+        String compareType = ""; // compare type
+        String sortingAlgorithm = ""; // sorting algorithm
 
-        String fileName = ""; // file name
-        boolean isDescending = false;
-        boolean isVerbose = false;
-        String sortAlgorithm = "";
-
-        // Parse command line arguments
-        for (int i = 0; i < args.length; i++) {
-            String arg = args[i].toLowerCase();
-    
-            if (arg.equals("-f")) {
-
-                fileName = args[i + 1];
-
-            } else if (arg.equals("-t")) {
-
-
-
-                String type = args[i + 1].toLowerCase();
-    
-                if (type.equals("v")) {
-                    isVerbose = true;
-                } else if (type.equals("h")) {
-                    // Sort by height
-                    sortAlgorithm = "height";
-                } else if (type.equals("a")) {
-                    // Sort by base area
-                    sortAlgorithm = "base area";
+        try {
+            for (int i = 0; i < args.length; i++) { // parse command line arguments
+                switch (args[i].toLowerCase()) { // convert to lowercase to make args case insensitive
+                    case "-f": // set path to file
+                        filePath = args[i + 1];
+                        break;
+                    case "-t": // select compare type
+                        compareType = args[i + 1];
+                        break;
+                    case "-s": // select sorting algorithm
+                        sortingAlgorithm = args[i + 1];
+                        break;
+                    default:
+                        System.out.println("Error: Invalid argument. Please use the format: -f <file location> -t <compare type> -s <algorithm>.\nTry adding a space after arguments.\nThe order of the arguments does not matter.");
+                        break;
                 }
-
-
-
-            } else if (arg.equals("-s")) {
-
-
-
-                String type = args[i + 1].toLowerCase();
-    
-                if (type.equals("b")) {
-                    // Bubble sort
-                    // sortAlgorithm = "bubble";
-                } else if (type.equals("s")) {
-                    // Selection sort
-                    // sortAlgorithm = "selection";
-                } else if (type.equals("i")) {
-                    // Insertion sort
-                    // sortAlgorithm = "insertion";
-                } else if (type.equals("m")) {
-                    // Merge sort
-                    // sortAlgorithm = "merge";
-                } else if (type.equals("q")) {
-                    // Quick sort
-                    // sortAlgorithm = "quick";
-                } else if (type.equals("z")) {
-                    // Your choice of sorting algorithm
-                    // sortAlgorithm = "your choice";
-                }
-
-
-
             }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
-        
+
+        String compareDimension = ""; // compare dimension
+        switch (compareType) { // check user input for errors and set shape dimension to compare if no errors
+            case "h":
+                compareDimension = "h";
+                System.out.println("Height");
+                break;
+            case "v":
+                compareDimension = "v";
+                System.out.println("Volume");
+                break;
+            case "a":
+                compareDimension = "a";
+                System.out.println("Base area");
+                break;
+            default:
+                System.out.println("Error: Invalid compare type. Please use one of the following: base, volume.");
+                break;
+        }
+
+        String selectedAlgorithm = ""; // selected algorithm
+        switch (sortingAlgorithm) { // check user input for errors and select the algorithm if no errors
+            case "b":
+                selectedAlgorithm = "b";
+                System.out.println("Bubble sort");
+                break;
+            case "s":
+                selectedAlgorithm = "s";
+                System.out.println("Selection sort");
+                break;
+            case "i":
+                selectedAlgorithm = "i";
+                System.out.println("Insertion sort");
+                break;
+            case "m":
+                selectedAlgorithm = "m";
+                System.out.println("Merge sort");
+                break;
+            case "q":
+                selectedAlgorithm = "q";
+                System.out.println("Quick sort");
+                break;
+            case "z":
+                selectedAlgorithm = "z";
+                System.out.println("custom sort");
+                break;
+            default:
+                System.out.println("Error: Invalid sorting algorithm. Please use one of the following: bubble, insertion, selection, merge, quick.");
+                break;
+        }
+
         // READ FILE
-        File file = new File("./src/application/resources/test1.txt"); // file path of file to read 
+        File file = new File(filePath); // file path from command line argument -f
         Object[] shapeObjectArray = (Object[]) ReadFiles.ReadFile(file); // read file and store shape arrays as objects in object array
         
         // cast object[n] in object array to shape array 
@@ -92,52 +104,41 @@ public class App {
         PentagonalPrism[] pentagonalPrismArray = (PentagonalPrism[]) shapeObjectArray[6];
         OctagonalPrism[] octagonalPrismArray = (OctagonalPrism[]) shapeObjectArray[7];
 
-        
+        // Cone cone = new Cone(674.2435, 652.1534);
+        // Cylinder cylinder = new Cylinder(9431.453, 4450.123);
 
+        // // cannot use ArrayList
+        // ArrayList<Shape> shapes = new ArrayList<Shape>();
+        // shapes.add(cylinder);
+        // shapes.add(cone);
 
+        // Collections.sort(shapes);
+        // for (Shape l : shapes) {
+        //     System.out.println(l);
+        // }
 
-
-
-
-
-        Cone cone = new Cone(674.2435, 652.1534);
-        Cylinder cylinder = new Cylinder(9431.453, 4450.123);
-
-        // cannot use ArrayList
-        ArrayList<Shape> shapes = new ArrayList<Shape>();
-        shapes.add(cylinder);
-        shapes.add(cone);
-
-        Collections.sort(shapes);
-        for (Shape l : shapes) {
-            System.out.println(l);
-        }
-
-        Comparator<Shape> comparator = new Comparator<Shape>() {
-            @Override
-            public int compare(Shape s1, Shape s2) {
-                double area1 = s1.calcBaseArea();
-                double area2 = s2.calcBaseArea();
-                if (area1 < area2) {
-                    return -1;
-                } else if (area1 > area2) {
-                    return 1;
-                } else {
-                    double volume1 = s1.calcVolume();
-                    double volume2 = s2.calcVolume();
-                    if (volume1 < volume2) {
-                        return -1;
-                    } else if (volume1 > volume2) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                }
-            }
-        };
-        Collections.sort(shapes, comparator);
-
-        
-        consoleScanner.close(); // close scanner
+        // Comparator<Shape> comparator = new Comparator<Shape>() {
+        //     @Override
+        //     public int compare(Shape s1, Shape s2) {
+        //         double area1 = s1.calcBaseArea();
+        //         double area2 = s2.calcBaseArea();
+        //         if (area1 < area2) {
+        //             return -1;
+        //         } else if (area1 > area2) {
+        //             return 1;
+        //         } else {
+        //             double volume1 = s1.calcVolume();
+        //             double volume2 = s2.calcVolume();
+        //             if (volume1 < volume2) {
+        //                 return -1;
+        //             } else if (volume1 > volume2) {
+        //                 return 1;
+        //             } else {
+        //                 return 0;
+        //             }
+        //         }
+        //     }
+        // };
+        // Collections.sort(shapes, comparator);
     }
 }
